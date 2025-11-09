9e25bb2b1946d2250fda9d5dde6f256345320b0a
orbb4 <reiturra2021@udec.cl>
2025-11-09 19:21:11 -0300

    Initial commit

---
+++ app/src/main/java/com/example/maestrochasquilla/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.maestrochasquilla
+
+import WorkerRequestViewModel
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.maestrochasquilla.ui.theme.MaestroChasquillaTheme
+
+import retrofit2.http.GET
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MaestroChasquillaTheme {
+                    WorkerRequestsDisplayer()
+
+                }
+            }
+
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MaestroChasquillaTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun WorkerRequestsDisplayer() {
+    val viewModel: WorkerRequestViewModel = viewModel()
+    val advice = viewModel.workerRequest.value
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = advice,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(16.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { viewModel.fetchWorkerRequests() }) {
+            Text("Refrescar solicitudes")
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/maestrochasquilla/WorkerRequestViewModel.kt
@@ -0,0 +1,48 @@
+import android.R
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import androidx.compose.runtime.mutableStateOf
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+
+
+class WorkerRequestViewModel : ViewModel() {
+    val workerRequest = mutableStateOf("Fetching worker request...")
+
+    init {
+        fetchWorkerRequests()
+    }
+
+    fun fetchWorkerRequests() {
+        viewModelScope.launch {
+            try {
+                val response = RetrofitInstance.api.getWorkerRequests()
+
+                //And Remove the advice with _advice
+                workerRequest.value = response
+            } catch (e: Exception) {
+                workerRequest.value = "Error: ${e.message}"
+            }
+        }
+    }
+}
+object RetrofitInstance {
+    private const val BASE_URL = "http://192.168.1.95:8000/"
+
+    val api: AdviceApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(retrofit2.converter.scalars.ScalarsConverterFactory.create())
+            .build()
+            .create(AdviceApiService::class.java)
+    }
+}
+interface AdviceApiService {
+    @GET("workerrequests")
+    suspend fun getWorkerRequests(): String
+}
+
